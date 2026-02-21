9fed12f83e1f7f25555b85c869ff86970adfb985
Hexpertify <hexpertifyapp@gmail.com>
2026-02-21 21:08:21 +0530

    Enable experimental server actions in config

--- next.config.js
+++ next.config.js
@@ -5,11 +5,11 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
-  // experimental: {
-  //   serverActions: {
-  //     allowedOrigins: ['localhost:3000', 'localhost:3001', 'scaling-spork-jj9jvg9rvrjq2pv4p-3001.app.github.dev'],
-  //   },
-  // },
+  experimental: {
+    serverActions: {
+      allowedOrigins: ['localhost:3000', 'localhost:3001', 'scaling-spork-jj9jvg9rvrjq2pv4p-3001.app.github.dev'],
+    },
+  },
 };
 
 module.exports = nextConfig;
