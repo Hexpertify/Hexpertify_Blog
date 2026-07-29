706f3d0a63d9e49afa56a05d68ea04219fc186cb
Ranjani <ranjani@dmice.ac.in>
2026-07-29 16:25:59 +0000

    Breadcrumb issue

--- components/blog/BlogDetailHero.tsx
+++ components/blog/BlogDetailHero.tsx
@@ -30,14 +30,6 @@ export default function BlogDetailHero({ blog }: BlogDetailHeroProps) {
           Blog
         </Link>
         <span>›</span>
-        <span className="flex flex-wrap items-center gap-2">
-          {(blog.categories && blog.categories.length > 0 ? blog.categories : [blog.category].filter(Boolean)).map((category) => (
-            <span key={category} className="rounded-full border border-gray-300 px-2.5 py-0.5 text-gray-700">
-              {category}
-            </span>
-          ))}
-        </span>
-        <span>›</span>
         <span className="text-gray-900 break-words">{blog.title}</span>
       </div>
 
